48315019bce7fa247c191e648548edb8a43fecbd
Nazmul Alam <shawonkst15@gmail.com>
2022-02-08 10:14:43 +0600

    add new

--- js/js-dom-1.js
+++ js/js-dom-1.js
@@ -2,4 +2,7 @@ function makeRed(){
     document.body.style.backgroundColor='red';
     document.body.style.color='white';
     document.body.style.textAlign='center';
+    document.body.style.border='1px solid black';
+    document.body.style.margin='10px ';
+    document.body.style.padding='10px ';
 }
